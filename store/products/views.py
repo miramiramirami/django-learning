b7647539b2ae07329983b169885d4d91396667f3
miramiramirami <xeless@mail.ru>
2025-06-04 20:27:09 +0500

    шаблон index base products DRY

--- store/products/views.py
+++ store/products/views.py
@@ -4,11 +4,35 @@ from django.shortcuts import render
 
 def index(request):
     context = {'title': 'store',
-               'username': 'nickname',
     }
     return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {'image': 'static/vendor/img/products/Adidas-hoodie.png',
+            'name': 'Худи черного цвета с монограммами adidas Originals',
+             'price': 6090,
+             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {'image': 'static/vendor/img/products/Adidas-hoodie.png',
+             'name': 'Худи черного цвета с монограммами adidas Originals',
+             'price': 6090,
+             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+             },
+            {'image': 'static/vendor/img/products/Adidas-hoodie.png',
+             'name': 'Худи черного цвета с монограммами adidas Originals',
+             'price': 6090,
+             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+             },{'image': 'static/vendor/img/products/Adidas-hoodie.png',
+            'name': 'Худи черного цвета с монограммами adidas Originals',
+             'price': 6090,
+             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это обраfffffffffз жизни.',
+            }
+        ]
+
+    }
+    return render(request, 'products/products.html', context)
 
