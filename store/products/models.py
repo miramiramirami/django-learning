dbfbc99fb84027fb2f7353b6faded9e28942f5c3
miramiramirami <xeless@mail.ru>
2025-06-06 17:49:08 +0500

    модели и миграции

--- store/products/models.py
+++ store/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    image = models.ImageField(upload_to='products/products_images')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
 
