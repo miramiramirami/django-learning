cc63e3b59c7cb75aa0c110e735e354e925af955a
miramiramirami <xeless@mail.ru>
2025-06-12 14:34:29 +0500

    страница профиля и редактирование данных

--- store/users/urls.py
+++ store/users/urls.py
@@ -6,5 +6,6 @@ app_name = 'users'
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
+    path('profile/', profile, name='profile'),
 ]
 
--- store/users/views.py
+++ store/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
 from django.contrib import auth
 
 def login(request):
@@ -30,5 +30,16 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+    context = {'title': 'Store - Профиль', 'form': form}
+    return render(request, 'users/profile.html', context)
 
 
